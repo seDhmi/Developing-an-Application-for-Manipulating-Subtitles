088c6135cffb89eab3f781cbae6421dc995889a5
abdulmajeedsaleh <99abdulmajeed@gmail.com>
2017-12-19 00:47:42 +0300

    Update TimeInterval.java

--- TimeInterval.java
+++ TimeInterval.java
@@ -1,19 +1,25 @@
 public class TimeInterval implements Comparable<TimeInterval> {
-	private Time startTime;
-	private Time endTime;
 	
-	public TimeInterval (Time start,Time end){
-		this.startTime=start;
-		this.endTime=end;
+	
+	private CTime startTime;
+	private CTime endTime;
+	
+	public TimeInterval (Time time,Time time2){
+		startTime= new CTime(time.getHH(),time.getMM(),time.getSS(),time.getMS());
+		endTime= new CTime(time2.getHH(),time2.getMM(),time2.getSS(),time2.getMS());
 	}
+	
+
 	@Override
 	public int compareTo(TimeInterval that) {
-		if ( ((TimeInterval) startTime).compareTo((TimeInterval) that.endTime) > 0) {
+		if (((startTime).compareTo(that.endTime)) > 0) {
 			return 1;
 		}
-		if (((TimeInterval) endTime).compareTo((TimeInterval) that.startTime) < 0) {
+		if (((endTime).compareTo(that.startTime)) < 0) {
 			return -1;
 		}
 		return 0;
 	}
+
+	 
 }
